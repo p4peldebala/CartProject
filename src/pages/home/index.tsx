f6c5a0f2bc5ebd43487f16d68fcd9de2836b51c6
Diogo Eduardo <dev.juniordiogo@gmail.com>
2024-01-30 02:41:18 -0300

    uptade

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -49,7 +49,14 @@ export const Home = () => {
     }
     return (
       <>
-        {productAdded && <div className='h-10  animate-bounce mx-auto flex items-center rounded-lg mt-5 text-slate-600 font-bold justify-center w-96 bg-green-300'>Produto adicionado com sucesso</div>}
+        {productAdded && (
+          <div className='fixed inset-x-0 top-0'>
+            <div className="h-10  animate-bounce mx-auto flex items-center rounded-lg mt-5 text-slate-600 font-bold justify-center w-96 bg-green-300">
+              Produto adicionado com sucesso
+            </div>
+          </div>
+          
+        )}
         <main className="w-full max-w-7xl px-4 mx-auto">
           <h1 className="text-center font-bold my-8 text-2xl">
             Produtos em alta
@@ -58,20 +65,30 @@ export const Home = () => {
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-6">
             {produtos.map((produto) => {
               return (
-                <CardItem key={produto.id} className="w-full flex items-center flex-col">
+                <CardItem
+                  key={produto.id}
+                  className="w-full flex items-center flex-col"
+                >
                   <img
                     className="w-full h-max-70 rounded-lg max-h-72 mb-2 p-6"
                     src={produto.cover}
                   />
-                  <p className="font-medium my-2 text-center p-2 h-10">{produto.title}</p>
+                  <p className="font-medium my-2 text-center p-2 h-10">
+                    {produto.title}
+                  </p>
                   <div className="flex gap-3 items-center m-4">
-                    <strong className="text-zinc-600">{priceFormater.format(produto.price)}</strong>
-                    <button onClick={() => handleAddNewProductToCart(produto)} className="bg-zinc-900 p-1 rounded">
+                    <strong className="text-zinc-600">
+                      {priceFormater.format(produto.price)}
+                    </strong>
+                    <button
+                      onClick={() => handleAddNewProductToCart(produto)}
+                      className="bg-zinc-900 p-1 rounded"
+                    >
                       <BsCartPlus size={20} color="#fff" />
                     </button>
                   </div>
                 </CardItem>
-              )
+              );
             })}
           </div>
         </main>
